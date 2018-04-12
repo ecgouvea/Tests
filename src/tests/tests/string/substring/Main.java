3275037a631b94d27136633452dedb8b366cddc6
eduardo.gouvea <eduardo.gouvea-smp@parceiros-kroton.com.br>
2018-04-12 19:15:10 -0300

    Throws StringIndexOutOfBoundsException.

--- src/tests/tests/string/substring/Main.java
+++ src/tests/tests/string/substring/Main.java
@@ -3,6 +3,7 @@ package tests.tests.string.substring;
 public class Main {
 
     public static void main(String[] args) {
+        // Will throw StringIndexOutOfBoundsException
         System.out.println("1234567890".substring(0, 11));
     }
 }
