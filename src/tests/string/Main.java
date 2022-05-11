27e14de5983eb2cfa5815d94b3ec1773829c3bb9
ecgouvea <ecgouvea@hotmail.com>
2022-05-11 16:44:28 -0300

    Test with String

---
+++ src/tests/string/Main.java
@@ -0,0 +1,10 @@
+package tests.string;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s = null;
+
+        System.out.println("Append string s " + s);
+    }
+}
