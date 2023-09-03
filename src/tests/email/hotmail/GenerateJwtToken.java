847ea72b32ce8a4d3dc8ef22f1273ef48f9961e6
egouvea <egouvea@avaya.com>
2023-09-03 12:53:06 -0300

    List Hotmail e-mail messages

---
+++ src/tests/email/hotmail/GenerateJwtToken.java
@@ -0,0 +1,63 @@
+package tests.email.hotmail;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GenerateJwtToken {
+    public static void main(String[] args) {
+        String clientId = "<clientId>";
+        String clientSecret = "<clientSecret>";
+        String tenantId = "<tenantId>";
+
+        String accessToken = generateAccessToken(clientId, clientSecret, tenantId);
+
+        System.out.println("Generated Access Token: " + accessToken);
+    }
+
+    private static String generateAccessToken(String clientId, String clientSecret, String tenantId) {
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("iss", clientId);
+        claims.put("sub", clientId);
+        claims.put("aud", "https://graph.microsoft.com");
+        claims.put("exp", new Date(nowMillis + 3600000)); // 1 hour validity
+        claims.put("iat", now);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .signWith(SignatureAlgorithm.HS256, clientSecret)
+                .compact();
+    }
+}
+
+
+/*
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+public class GenerateJwtToken {
+    public static void main(String[] args) {
+        String clientId = "<clientId>";
+        String clientSecret = "<clientSecret>";
+
+        String token = Jwts.builder()
+                .setSubject(clientId)
+                .setExpiration(new Date(System.currentTimeMillis() + 3600000)) // 1 hour validity
+                .signWith(SignatureAlgorithm.HS256, key)
+                .compact();
+
+        System.out.println("Generated JWT token: " + token);
+    }
+}
+*/
--- src/tests/email/hotmail/ListEmailsByDateRange.java
+++ src/tests/email/hotmail/ListEmailsByDateRange.java
@@ -1,41 +1,43 @@
 package tests.email.hotmail;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.util.EntityUtils;
-import org.apache.http.HttpResponse;
-import org.apache.http.Header;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.message.BasicHeader;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
 
-public class ListEmailsByDateRangeExample {
+public class ListEmailsByDateRange {
     public static void main(String[] args) throws Exception {
-        String accessToken = "YOUR_ACCESS_TOKEN";
-        String startDateTime = "2023-01-01T00:00:00Z"; // Replace with your desired start date
-        String endDateTime = "2023-08-31T23:59:59Z";   // Replace with your desired end date
+        String accessToken = "<accessToken>";
+        String startDateTime = "2023-08-16T00:00:00Z"; // Replace with your desired start date
+        String endDateTime = "2023-08-15T22:59:59Z";   // Replace with your desired end date
 
-        String apiUrl = "https://graph.microsoft.com/v1.0/me/messages" +
-                        "?$filter=receivedDateTime ge " + startDateTime +
-                        " and receivedDateTime le " + endDateTime;
+        String apiUrl = "https://graph.microsoft.com/v1.0/users/" +
+                        "<userID>" +
+                        "/messages" +
+                        "?$filter=receivedDateTime+ge+" + URLEncoder.encode(startDateTime, StandardCharsets.UTF_8) +
+                        "+and+receivedDateTime+le+" + URLEncoder.encode(endDateTime, StandardCharsets.UTF_8);
 
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet(apiUrl);
+        HttpClient client = HttpClient.newHttpClient();
 
-        // Set authorization header
-        httpGet.setHeader("Authorization", "Bearer " + accessToken);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(apiUrl))
+                .header("Authorization", "Bearer " + accessToken)
+                .build();
 
-        // Execute the request
-        HttpResponse response = httpclient.execute(httpGet);
-        String responseBody = EntityUtils.toString(response.getEntity());
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        // Parse the JSON response to extract email messages
-        // Sample code to parse JSON and extract email messages
-        System.out.println(responseBody);
-
-        httpclient.close();
+        if (response.statusCode() == 200) {
+            String responseBody = response.body();
+            System.out.println(responseBody);
+        } else {
+            System.out.println("Error: " + response.statusCode());
+            System.out.println("Error body: " + response.body());
+        }
     }
 }
